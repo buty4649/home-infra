35bc223822ab322cc2662c9fcb76b5f31393ec64
buty <buty4649@gmail.com>
2022-12-02 23:24:49 +0900

    Add b-server3(RPi4)

--- recipes/microk8s/default.rb
+++ recipes/microk8s/default.rb
@@ -1,3 +1,29 @@
+if node['machine'] == 'Raspberry Pi 4'
+  # need cgroup
+  # see. https://ubuntu.com/tutorials/how-to-kubernetes-cluster-on-raspberry-pi#4-installing-microk8s
+  file '/boot/firmware/cmdline.txt' do
+    action :edit
+    block do |content|
+      options = 'cgroup_enable=memory cgroup_memory=1'
+      unless content.include?(options)
+        content.insert(0, "#{options} ")
+      end
+    end
+    notifies :run, 'local_ruby_block[need_restart]', :immediately
+  end
+
+  local_ruby_block 'need_restart' do
+    action :nothing
+    block do
+      STDERR.puts "\e[31m**IMPORTANT**\e[0m"
+      STDERR.puts "\e[31mmicrok8s need cgroup.\e[0m"
+      STDERR.puts "\e[31mA reboot is required to activate the cgroup.\e[0m"
+      STDERR.puts
+      exit
+    end
+  end
+end
+
 snap_package 'microk8s' do
   classic true
   channel '1.25'
@@ -8,13 +34,15 @@ add_group 'microk8s' do
   user local_user
 end
 
-# fix: overlayfs: filesystem on '~' not supported as upperdir
-file '/var/snap/microk8s/current/args/containerd-template.toml' do
-  action :edit
-  block do |content|
-    content.gsub!('snapshotter = "${SNAPSHOTTER}"', 'snapshotter = "native"')
+if node['machine'] == 'NanoPi R4S'
+  # fix: overlayfs: filesystem on '~' not supported as upperdir
+  file '/var/snap/microk8s/current/args/containerd-template.toml' do
+    action :edit
+    block do |content|
+      content.gsub!('snapshotter = "${SNAPSHOTTER}"', 'snapshotter = "native"')
+    end
+    notifies :run, 'execute[Restart microk8s]'
   end
-  notifies :run, 'execute[Restart microk8s]'
 end
 
 execute 'Restart microk8s' do
