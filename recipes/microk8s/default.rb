8f51a7de4a579f4a0141ac32491a4d6ad46c7854
buty <buty4649@gmail.com>
2022-12-18 10:29:53 +0900

    パーティションを工夫して回避したので設定を削除

--- recipes/microk8s/default.rb
+++ recipes/microk8s/default.rb
@@ -37,17 +37,6 @@ add_group 'microk8s' do
   user local_user
 end
 
-if node['machine'] == 'NanoPi R4S'
-  # fix: overlayfs: filesystem on '~' not supported as upperdir
-  file '/var/snap/microk8s/current/args/containerd-template.toml' do
-    action :edit
-    block do |content|
-      content.gsub!('snapshotter = "${SNAPSHOTTER}"', 'snapshotter = "native"')
-    end
-    notifies :run, 'execute[Restart microk8s]'
-  end
-end
-
 execute 'Restart microk8s' do
   action :nothing
   command 'microk8s stop; microk8s start'
