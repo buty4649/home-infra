b3b534b8b914262ffe35443d37cd090a8d26a691
buty4649 <buty4649@gmail.com>
2023-08-20 17:58:35 +0900

    include_recipeっぽくかけるようにした

--- attributes.rb
+++ attributes.rb
@@ -15,6 +15,17 @@ def load_properties(role)
   return unless File.exist?(role_file)
 
   override_properties = YAML.load_file(role_file)
+  if recipes = override_properties.delete('recipes')
+    override_properties['run_list'] ||= []
+    recipes.each do |recipe|
+      basepath = File.join('.', 'recipes', recipe.split("::"))
+      recipe_file = ["#{basepath}.rb", File.join(basepath, 'default.rb')].find do |path|
+        File.exist?(path)
+      end
+      raise "recipe #{recipe} not found" unless recipe_file
+      override_properties['run_list'] << recipe_file
+    end
+  end
   host.properties.merge!(override_properties)
 end
 
