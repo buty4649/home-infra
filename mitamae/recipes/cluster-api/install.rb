30ff3d402f535226204a85abb3aef151a9db4dc1
buty4649 <buty4649@gmail.com>
2023-08-21 00:16:36 +0900

    cluster-apiのインストールレシピを追加

---
+++ mitamae/recipes/cluster-api/install.rb
@@ -0,0 +1,13 @@
+install_path = node.dig('cluster-api', 'install_path') || '/usr/local/bin/cluster-api'
+version = node['cluster-api']['version']
+
+execute 'Download cluster-api binary' do
+  command <<~COMMAND
+    wget https://github.com/kubernetes-sigs/cluster-api/releases/download/v#{version}/clusterctl-linux-amd64 -O #{install_path}
+  COMMAND
+  not_if "test -x #{install_path} && #{install_path} version -o short | grep -q v#{version}"
+end
+
+file install_path do
+  mode '0755'
+end
