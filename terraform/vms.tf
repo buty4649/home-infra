8f7d92f83fbf1d345a27cdef6d0eec97f9c0228b
buty4649 <buty4649@gmail.com>
2023-08-20 22:54:58 +0900

    k8sクラスタ用のノードを追加

--- terraform/vms.tf
+++ terraform/vms.tf
@@ -7,18 +7,32 @@ terraform {
 }
 
 locals {
-  vms         = ["cluster-api"]
+  vms = {
+    cluster-api       = {},
+    k8s-control-plane = {},
+    k8s-worker-1 = {
+      disk_size = "256G",
+      memory    = 16384,
+    },
+    k8s-worker-2 = {
+      disk_size = "256G",
+      memory    = 16384,
+    },
+  }
   target_node = "miyakojima"
 }
 
 module "vm" {
   source = "./modules/vm"
 
-  for_each = toset(local.vms)
+  for_each = local.vms
 
   name        = each.key
   domain      = "b-net.local"
   target_node = local.target_node
+
+  memory    = lookup(each.value, "memory", 8192)
+  disk_size = lookup(each.value, "disk_size", "72G")
 
   pve_host             = var.pve_host
   pve_user             = var.pve_user
