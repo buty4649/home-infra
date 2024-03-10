d3d04c244331d65879eb5fd0ad5038886cac5bf0
buty4649 <buty4649@gmail.com>
2024-03-10 21:40:59 +0900

    Exclude PerlBackrefs in .rubocop.yml, update /etc/hosts

--- mitamae/recipes/hostname/default.rb
+++ mitamae/recipes/hostname/default.rb
@@ -3,3 +3,10 @@ execute 'set hostname' do
   command "hostnamectl set-hostname #{hostname}"
   not_if "test `hostnamectl --static` = '#{hostname}'"
 end
+
+file '/etc/hosts' do
+  action :edit
+  block do |content|
+    content.gsub!(/^(127.0.1.1\s+).+$/, "\\1#{hostname}")
+  end
+end
