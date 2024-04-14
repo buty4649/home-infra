ba2fc0d1b50a7df33fb60613cea57650e35d0627
buty4649 <buty4649@gmail.com>
2024-04-14 15:31:29 +0900

    Update bird

--- mitamae/recipes/bird/default.rb
+++ mitamae/recipes/bird/default.rb
@@ -1,16 +1,24 @@
 package 'bird2'
 
+local_ipv4 = node['local-ipv4'].split('.')
+
+# local_ipv4の一つ前がuplink_ip
+uplink_ip = local_ipv4[0..2].push(local_ipv4.last.to_i - 1)
+
 template '/etc/bird/bird.conf' do
   owner 'bird'
   group 'bird'
   mode '0640'
   variables(
-    router_id: node['local-ipv4'],
-    local_community: "65002,#{node['local-ipv4'].split('.').last}",
-    neighbor: '192.168.177.1',
-    neighbor_asn: 65_001,
-    local_asn: 65_002,
-    cilium_asn: 65_003
+    router_id: local_ipv4.join('.'),
+    community_tag: local_ipv4.last,
+    uplink_ip: uplink_ip.join('.'),
+    uplink_asn: 65_001,
+    my_asn: 65_002,
+    cilium_asn: 65_003,
+    pod_cidr: '172.16.0.0/16',
+    service_cidr: '172.16.252.0/22',
+    external_cidr: '192.168.177.64/27'
   )
   notifies :reload, 'service[bird]'
 end
